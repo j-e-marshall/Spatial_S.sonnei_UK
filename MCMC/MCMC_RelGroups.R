5267891a85fc18badb4e26e57f63916bf572db46
j-e-marshall <j.marshall@yale.edu>
2026-06-22 12:34:35 -0400

    Update MCMC_RelGroups.R

--- MCMC/MCMC_RelGroups.R
+++ MCMC/MCMC_RelGroups.R
@@ -204,9 +204,11 @@ datindep[2,3] <-quantile(t1reltrav, c(0.025))
 datindep[2,4]<- quantile(t1reltrav, c(0.975))
 datindep[2,5]<- quantile(t1reltrav, c(0.50))
 
-
 plotgrowth<- datindep %>% ggplot(aes(x= Group, y = exp(Mean))) + geom_point(aes(colour=Group)) + 
-  geom_errorbar(aes(ymin=exp(q2.5), ymax=exp(q972.5), ,colour=Group), width = 0.2) + theme_classic()  + labs(title = "Annual Growth Rate by Group Relative to Travel", y = "Annual Growth Rate", x = "Group")+ scale_y_continuous("Annual Growth Rate Relative to Travel", expand = c(0, 0), limits = c(-.2, .3))  + theme_classic()  + geom_hline(yintercept=0,  linetype="dashed", color ="#f85c06")+ scale_color_manual(values = c("#b21819", "#088978"))
+  geom_errorbar(aes(ymin=exp(q2.5), ymax=exp(q972.5),colour=Group), width = 0.2) + 
+  scale_y_continuous(trans = "log", limits = c(0.95, 1.4), breaks = c(1, 1.25))+
+  theme_classic()  + labs(title = "Annual Growth Rate by Group Relative to Travel", y = "Annual Growth Rate", x = "Group") + 
+  geom_hline(yintercept=1,  linetype="dashed", color ="#f85c06")+ scale_color_manual(values = c("#b21819", "#088978"))
 
 ############################################################################################
 ## Plot fits for model, per group
